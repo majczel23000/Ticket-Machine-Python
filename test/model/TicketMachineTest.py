8853ccf9131a56ed52ce7c67383dfd57e2ad143a
majczel23000 <michal.razny23@gmail.com>
2020-06-13 23:38:29 +0200

    test: view helper

--- test/model/TicketMachineTest.py
+++ test/model/TicketMachineTest.py
@@ -1,18 +1,22 @@
 import sys
 import unittest
-
 from PyQt5 import QtWidgets
-
 from src.model.TicketMachine import TicketMachine
 
 
 class TicketMachineTest(unittest.TestCase):
 
+    __ticketMachine = None
+
+    @classmethod
+    def setUp(cls):
+        app = QtWidgets.QApplication(sys.argv)
+        cls.__ticketMachine = TicketMachine()
+
     def test_singleton(self):
         app = QtWidgets.QApplication(sys.argv)
-        instance1 = TicketMachine()
-        instance2 = TicketMachine()
-        self.assertTrue(instance1 == instance2)
+        instance = TicketMachine()
+        self.assertTrue(instance == self.__ticketMachine)
 
 
 if __name__ == '__main__':
---
+++ test/view/ViewHelperTest.py
@@ -0,0 +1,53 @@
+import sys
+
+from PyQt5 import QtWidgets
+
+from src.view.ViewHelper import ViewHelper
+import unittest
+
+
+class ViewHelperTest(unittest.TestCase):
+    __viewHelper = None
+    __view = None
+    button_name = 'button_test'
+    label_name = 'label_test'
+    qvbox_name = 'qvbox_test'
+    qgrid_name = 'qgrid_test'
+    qwidget_name = 'qwidget_test'
+
+    @classmethod
+    def setUp(cls):
+        cls.__viewHelper = ViewHelper()
+
+    def test_find_button_by_object_name(self):
+        self.helper_create_view()
+        self.assertIsInstance(self.__viewHelper.find_button_by_object_name(self.__view, self.button_name), QtWidgets.QPushButton)
+
+    def test_find_label_by_object_name(self):
+        self.helper_create_view()
+        self.assertIsInstance(self.__viewHelper.find_label_by_object_name(self.__view, self.label_name), QtWidgets.QLabel)
+
+    def test_find_QVBoxLayout_by_object_name(self):
+        self.helper_create_view()
+        self.assertIsInstance(self.__viewHelper.find_QVBoxLayout_by_object_name(self.__view, self.qvbox_name), QtWidgets.QVBoxLayout)
+
+    def test_find_QWidget_by_object_name(self):
+        self.helper_create_view()
+        self.assertIsInstance(self.__viewHelper.find_QWidget_by_object_name(self.__view, self.qwidget_name), QtWidgets.QWidget)
+
+    def helper_create_view(self):
+        app = QtWidgets.QApplication(sys.argv)
+        window = QtWidgets.QMainWindow()
+        widget = QtWidgets.QWidget()
+        widget.setObjectName(self.qwidget_name)
+        window.setCentralWidget(widget)
+        btn = QtWidgets.QPushButton(widget)
+        btn.setObjectName(self.button_name)
+        label = QtWidgets.QLabel(widget)
+        label.setObjectName(self.label_name)
+        qvbox = QtWidgets.QVBoxLayout(widget)
+        qvbox.setObjectName(self.qvbox_name)
+        qgrid = QtWidgets.QGridLayout(widget)
+        qgrid.setObjectName(self.qgrid_name)
+        self.__view = window
+
